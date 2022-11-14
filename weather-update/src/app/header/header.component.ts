fbf30e2253a5b74e343b496b5f9d17565721e83e
nagaveninaik <70188382+nagaveninaik@users.noreply.github.com>
2022-11-14 22:57:17 +0530

    nothing doing

--- weather-update/src/app/header/header.component.ts
+++ weather-update/src/app/header/header.component.ts
@@ -7,11 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 date:any;
+home=true;
+fev=false;
+search=false;
+search1 = false;
+recent=false;
+fev1=false;
+city='';
+width=0;
   constructor() { }
 
   ngOnInit(): void {
     this.date = new Date();
-    
+    this.width=window.innerWidth;
+
+    // if(this.id == 1){
+    //   this.home=true;
+    // } else if (this.id == 2 && this.width <500){
+    //   this.fev1=true;
+    // this.recent= false;
+    // this.fev = true;
+    // this.home = false;
+    // } else if(this.id == 3 && this.width < 500){
+    //   this.fev1=false;
+    // this.recent= true;
+    // this.fev = true;
+    // this.home = false;
+    // }
   }
 
+  navDisplayPermission(){}
+
+  searchBar(){
+
+  }
+  clear(){}
+
+  backHome(){}
+  submit(){
+  
+  }
+  clearBtn(){}
 }
