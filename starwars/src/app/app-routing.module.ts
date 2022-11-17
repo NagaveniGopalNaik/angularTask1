ca6c6bd166ac8d1139a9a4f420239e22795e39dc
NagaveniGopalNaik <nagaveni.naik@robosoftin.com>
2022-11-17 18:44:08 +0530

    ui complete

---
+++ starwars/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { CharacterComponent } from './character/character.component';
+
+const routes: Routes = [
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'home', component: HomeComponent},
+  {path:'details', component: DetailsComponent},
+  {path:'character', component: CharacterComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
